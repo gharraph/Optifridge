c13c1a8fd1f7a7e9fa086d5d9ee3384a787dec9b
Apprentice <apprentice@devbootcamp.com>
2012-08-06 16:13:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Optifridge::Application.config.secret_token = 'ce967ed4c6809154c275f537dad381c77c15c93d193fd3569380a240313ab7e654e35111b23bd0016d83eaae8a5063e12fb159fdadb026ce18e0e24e14c59fcc'
