1481b67b0ea21d7df67961d1fbc8b98f6109087d
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-08-07 00:31:38 -0700

    Styling play and adds root path to logo

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,15 +8,25 @@ describe 'Static pages' do
     describe "logged out" do
       before { visit root_path }
 
-      it { should have_content('Get the most out of your fridge.') }
-      it { should have_selector('a', :text => 'Optifridge') }
-      it { should have_selector('a', :text => 'Sign in') }
+      it { should     have_content('Get the most out of your fridge.') }
+      it { should     have_selector('a', :text => 'Optifridge') }
+      it { should     have_selector('a', :text => 'Sign in') }
       it { should_not have_selector('a', :text => 'Sign up') }
+      it { should     have_button("Let's boogie") }
 
       describe "clicks on Sign in" do
         before { click_link "Sign in" }
 
         it { should have_selector('h2', :text => 'Sign in') }
+      end
+      
+      describe "clicks on logo from non-root path" do
+        before do 
+          visit new_user_session_path
+          click_link "Optifridge" 
+        end
+        
+        it { should have_selector('h2', :text => 'Get the most out of your fridge.') }
       end
 
     end
