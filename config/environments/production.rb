6283c519b0a03d0fd27e69373a23c5f40f878d31
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-08-09 15:37:06 -0700

    Attempts to fix twitter bootstrap rails issue with favicons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Optifridge::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  # To fix favicons issues with twitter-bootstrap-rails
+  config.assets.precompile = [/^[-_a-zA-Z0-9]*\..*/]
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -64,4 +67,5 @@ Optifridge::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
 end
