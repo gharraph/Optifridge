f3c9efb2eb27008d19ac9ad8ddceec3058ab89f0
Paul Zaich <pzaich@gmail.com>
2012-08-14 21:35:47 -0700

    helper that adds the correct storage types to select

--- app/helpers/item_kinds_helper.rb
+++ app/helpers/item_kinds_helper.rb
@@ -1,9 +1,3 @@
 module ItemKindsHelper
-	def list_storage(types)
-		select_compiled = []
-		types.each do |storage_type|
-			select_compiled << [storage_type, storage_type]
-		end
-		select_compiled
-	end
+
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -13,4 +13,15 @@ module ItemsHelper
     end
   end
 
+  def list_all_storage_options(item_kind)
+    @locations = item_kind.shelf_lives.map { |shelf_life| shelf_life.location.name if !shelf_life.location.nil? }.compact
+  end
+
+  def insert_storage_options
+    if !@item_kind.nil?
+      list_all_storage_options(@item_kind)
+    else
+      ["Refrigerator", "Freezer", "Pantry"]
+    end
+  end
 end
