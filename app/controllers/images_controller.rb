d8fcee9e62c89b5c51542b1d50d2c237eaa48c32
Apprentice <apprentice@devbootcamp.com>
2012-08-16 12:43:33 -0700

    Increases testing and refactors image processing

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -21,25 +21,30 @@ class ImagesController < ApplicationController
 
   private
     def translate_image
-      send_image_for_translation
-    end
-
-    def send_image_for_translation
-      ocr_api_url = "http://svc.webservius.com/v1/wisetrend/wiseocr/submit?wsvKey=BMCxkJ6jPP15FBZdtJ5aY864KvRsxC1x"
+      #old key BMCxkJ6jPP15FBZdtJ5aY864KvRsxC1x
+      ocr_api_url = "http://svc.webservius.com/v1/wisetrend/wiseocr/submit?wsvKey=kbPy3KcUFj6s1IJQHnfshS7fSi_bpXXh"
       body_string = "<Job><InputURL>#{ @image.stored_at }</InputURL></Job>"
       options = { :body => body_string, :headers => { 'Content-type' => "text/xml" }, :timeout => 120 }
       get_translation(HTTParty.post(ocr_api_url, options))
+      @image.update_attributes(:translation => @raw_translation)
     end
 
     def get_translation(response)
       @job_uri = Nokogiri::XML(response.to_xml).xpath("//JobURL")
       while !image_done_processing?
-        warn ("*" * 50) + "\n" + "Looping again.."
+        warn ("*" * 50) + "\n" + "Checking OCR processing status again.."
         sleep 2
       end
-      job_uri_doc = Nokogiri::XML(open(@job_uri.children.text))
-      translation_raw = open(job_uri_doc.xpath('//Uri')[0].children.text).read
-      @image.update_attributes(:translation => translation_raw)
+      get_job_uri
+      get_raw_translation
+    end
+
+    def get_raw_translation
+      @raw_translation = open(@job_uri_doc.xpath('//Uri')[0].children.text).read
+    end
+
+    def get_job_uri
+      @job_uri_doc = Nokogiri::XML(open(@job_uri.children.text))
     end
 
     def image_done_processing?
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe Image do
+  let(:image) { Fabricate(:image) }
   it { should belong_to(:user) }
 
-
-  it "should be able to match parts of the translation to existing item_synonyms" do
-    Fabricate(:item_synonym)
-
-  end
-
+  # can't seem to get the file upload part of the fabricate working
+  it "has an amazon storage url based on its name" # do
+  #     synonym.stored_at.should eq "https://s3.amazonaws.com/optifridge_images/image/#{synonym.id}/#{synonym.name}"
+  #   end
 end
