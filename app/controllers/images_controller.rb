5d6add32de789fe5606ecee3cdc213413d36a53a
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-08-16 13:35:40 -0700

    Fixes new lines missing

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -45,7 +45,7 @@ class ImagesController < ApplicationController
     end
 
     def get_raw_translation
-      @raw_translation = open(@job_uri_doc.xpath('//Uri')[0].children.text).read.gsub(' ','_').gsub(/\W/, '').gsub('_',' ')
+      @raw_translation = open(@job_uri_doc.xpath('//Uri')[0].children.text).read.gsub('\n', 'nnn').gsub(' ','_').gsub(/\W/, '').gsub('_',' ').gsub('nnn', '\n')
     end
 
     def get_job_uri
